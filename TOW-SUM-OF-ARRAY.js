42ec9097466976409a3edf4b6bfbf03e05041a70
Mohammed Ihsaanul Haque <70464396+MohammedIhsaan@users.noreply.github.com>
2021-10-07 09:49:22 +0530

    Update TOW-SUM-OF-ARRAY.js

--- TOW-SUM-OF-ARRAY.js
+++ TOW-SUM-OF-ARRAY.js
@@ -1,1 +1,16 @@
+var twoSum = function(nums, target) {
+    
+    let resultArr = []
+ let result = nums.map((n,i,arr)=>{
+   for(let j = 0;j<arr.length;j++){    
+    if(i!==j){
+      if(n + arr[j]==target){
+       resultArr.push(i)
+       
+      }
 
+    }    
+   }
+ })
+    return resultArr
+};
